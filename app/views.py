e837316840be9f5854815284d2d3988994115ed5
Your Name <you@example.com>
2019-06-11 16:04:57 +0530

    Updated with updated GET, POST requests removed irrelvant files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 import os
 from app import app
-
+import json
 
 # Web page renders
 @app.route('/')
@@ -19,16 +19,18 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print("Request:")
     print(json.dumps(req, indent=4))
-    res = makeWebhookResult(req)
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+    response_json = {}
+    return json.dumps(response_json)
 
 
-@app.route('/storeBalance/<bal>')
-def storeData(bal):
-    fopen = open('balance','w')
-    fopen.write(bal)
-    fopen.close()
-    return 'Balance Updated, {}!'.format(bal)
+@app.route('/endpoint/<val>')
+def store_data(val):
+    dynamic_url = val
+    return 'Dynamic value sent, {}!'.format(dynamic_url)
+
+@app.route('/new_endpoint/')
+def get_endpoint():
+    print('Request Params:')
+    request_params_dict = dict(request.args)
+    print(json.dumps(request_params_dict,indent=4))
+    return 'Response with parameters: {}'.format(request_params_dict)
